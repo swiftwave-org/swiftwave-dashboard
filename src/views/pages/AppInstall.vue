4949e222ee1fca470e447cd3da9ca0c8f08fba15
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-06-23 16:26:17 +0530

    feat: ignore variables starting with IGNORE

--- src/views/pages/AppInstall.vue
+++ src/views/pages/AppInstall.vue
@@ -228,7 +228,7 @@ const setupSystem = () => {
 const isFormFilled = computed(() => {
   let variables = toRaw(formStateRef)
   for (const [key, value] of Object.entries(variables)) {
-    if (key.startsWith('IGNORE_NM_')) {
+    if (key.startsWith('IGNORE_')) {
       continue
     }
     if (!formStateRef[key]) {
