c1038ab7240be5eaaf0030b8c08a4fa5d33709b8
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-07-19 00:51:32 +0530

    feat: revamp application list with grouping support (#240)

--- src/views/components/PageBar.vue
+++ src/views/components/PageBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex w-full flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0">
+  <div class="flex w-full flex-col space-y-2 md:flex-row md:items-center md:justify-between md:space-y-0">
     <div>
-      <h2 class="text-lg font-semibold">
+      <p class="text-base font-medium">
         <slot name="title"></slot>
-      </h2>
-      <p class="mt-1 text-sm text-gray-700">
+      </p>
+      <p class="text-sm text-gray-700">
         <slot name="subtitle"></slot>
       </p>
     </div>
